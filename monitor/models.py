acc6e2139d8b562b64cdcecb0af6d1c9af7f3bfe
Gijs Molenaar <gijs@pythonic.nl>
2013-10-22 19:14:29 +0200

    start of model

--- monitor/models.py
+++ monitor/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Statistic(models.Model):
+    name = models.CharField(unique=True, max_length=40)
+
+
+class Line(models.Model):
+    statistic = models.ForeignKey(Statistic, related_name='lines')
+    moment = models.DateTimeField()
+
+
+class IntValue(models.Model):
+    statistic = models.ForeignKey(Line, related_name='int_values')
+    index = models.IntegerField()
+    value = models.IntegerField()
+
+
+class FloatValue(models.Model):
+    statistic = models.ForeignKey(Line, related_name='float_values')
+    index = models.IntegerField()
+    value = models.FloatField()
+
+
+class StrValue(models.Model):
+    statistic = models.ForeignKey(Line, related_name='str_values')
+    index = models.IntegerField()
+    value = models.CharField(max_length=40)
