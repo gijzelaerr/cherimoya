38fac4822035e61fb45605d633b17f3734d97040
Gijs Molenaar <gijs@pythonic.nl>
2013-11-11 14:01:31 +0100

    switch to flask

--- monitor/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from monitor.models import Line, IntValue, FloatValue, Statistic, StrValue
-
-admin.site.register(Line)
-admin.site.register(IntValue)
-admin.site.register(FloatValue)
-admin.site.register(Statistic)
-admin.site.register(StrValue)
